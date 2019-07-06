b8068224902252b58b87d0ed5b17da3519ac2579
Maykheld <maykhel.david@gmail.com>
2019-07-06 14:35:29 +0700

    Adding all files

---
+++ README.md
@@ -0,0 +1,40 @@
+# Whatsapp Floatbox
+
+A lightweight responsive whatsapp api chatbox for your website. Easy to use.
+Put a whatsapp floating chat box on your website.
+
+This project is my own base template that can easily customized. .
+
+## Example
+View this page for example <a href="http://maykheld.com/wafloatbox.html">DEMO</a>
+
+## Usege
+
+### Links and Scripts
+```
+<link rel='stylesheet' type='text/css' media='screen' href='css/wafloatbox-0.2.min.css'>
+<script src='js/jquery-3.4.1.min.js'></script>
+<script src='js/wafloatbox-0.2.js'></script>
+```
+### HTML Markup
+ 
+```
+    <div class="myk-wa">
+        <div class="myk-item" data-wanumber="62xxxxxxxx" data-waname="Jonny" data-wadivision="Technician" data-waava="images/user.png"></div>
+        <div class="myk-item" data-wanumber="92xxxxxxx" data-waname="Rachel" data-wadivision="Sales" data-waava="images/user.png"></div>
+        <!-- few more -->
+    </div>
+```
+### Initializing
+```
+<script>
+    $(document).ready(function(){
+        $(".myk-wa").WAFloatBox();
+    });
+</script>
+```
+
+## License
+
+This project is under MIT License
+That's it.
---
+++ js/wafloatbox-0.2.js
@@ -0,0 +1,49 @@
+/* MIT License */
+/* Copyright (c) 2019 Maykheld (http://www.maykheld.com) */
+jQuery.fn.extend({
+    WAFloatBox: function () {
+        var a = $(this)
+        a.prepend(`
+        <div class="myk-btn">
+            <img src="images/wa-icon.png" class="myk-wa-icon">
+        </div>
+        <div class="myk-panel"></div>
+        `)
+
+
+        var b = $(".myk-panel")
+        b.append(`
+        <div class="myk-panelhead">
+            <span class="myk-close">x</span>
+                <img src="images/wa-icon.png" class="myk-wa-icon" width="30px">
+            <h2 class="myk-paneltitle">Questions? </h2>
+        </div>
+        <div class="myk-panelbody"></div>
+        `)
+
+        $('.myk-item').each(function(){
+        //this wrapped in jQuery will give us the current .letter-q div
+        $('.myk-panelbody').append(`
+        <a href="https://wa.me/${$(this).data('wanumber')}?text=I'm%20inquiring%20about%20the%20apartment%20listing" class="myk-list">
+            <img src="${$(this).data('waava')}" class="myk-ava">
+            <div class="myk-number">
+                <p class="myk-who"><b>${$(this).data('wadivision')}</b></p>
+                <p class="myk-name">${$(this).data('waname')}</p>
+            </div>
+        </a>
+        `)
+
+
+    });
+
+    // trigger 
+    $(".myk-btn").click(function(){
+    $(".myk-panel").toggleClass("myk-show");
+    });
+    $(".myk-close").click(function(){
+    $(".myk-panel").toggleClass("myk-show");
+    });
+
+    }
+});
+
